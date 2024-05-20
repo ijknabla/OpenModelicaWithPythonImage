500536a6e9ab125413367bb8badc53bd7d9b054b
ijknabla <ijknabla@gmail.com>
2024-05-21 02:35:43 +0900

    Update main

--- omcpyimage/__main__.py
+++ omcpyimage/__main__.py
@@ -1,31 +1,19 @@
 from __future__ import annotations
 
-import asyncio
 from asyncio import Lock, TimeoutError, gather, wait_for
-from collections.abc import AsyncGenerator, Callable, Coroutine
+from collections.abc import AsyncGenerator
 from contextlib import AsyncExitStack, asynccontextmanager
-from functools import wraps
 from operator import itemgetter
-from typing import IO, Any, ParamSpec, TypeVar, TypeVarTuple
+from typing import IO
 
 import click
 import tomllib
+from PySide6 import QtAsyncio
+from PySide6.QtCore import QCoreApplication
 
 from . import builder
 from .builder import OpenmodelicaPythonImage
 from .config import Config
-
-P = ParamSpec("P")
-T = TypeVar("T")
-Ts = TypeVarTuple("Ts")
-
-
-def execute_coroutine(f: Callable[P, Coroutine[Any, Any, T]]) -> Callable[P, T]:
-    @wraps(f)
-    def wrapped(*args: P.args, **kwargs: P.kwargs) -> T:
-        return asyncio.run(f(*args, **kwargs))
-
-    return wrapped
 
 
 @click.command
@@ -35,45 +23,48 @@ def execute_coroutine(f: Callable[P, Coroutine[Any, Any, T]]) -> Callable[P, T]:
     type=click.File(mode="rb"),
 )
 @click.option("--limit", type=int, default=1)
-@execute_coroutine
-async def main(config_io: IO[bytes], limit: int) -> None:
-    config = Config.model_validate(tomllib.load(config_io))
+def main(config_io: IO[bytes], limit: int) -> None:
+    async def impl() -> None:
+        config = Config.model_validate(tomllib.load(config_io))
 
-    pythons = await builder.search_python_versions(config.python)
+        pythons = await builder.search_python_versions(config.python)
 
-    ubuntu_openmodelica = await builder.categorize_by_ubuntu_release(config.from_)
+        ubuntu_openmodelica = await builder.categorize_by_ubuntu_release(config.from_)
 
-    images = {
-        OpenmodelicaPythonImage(
-            base="ijknabla/openmodelica",
-            ubuntu=ubuntu,
-            openmodelica=openmodelica,
-            python=python,
-        )
-        for ubuntu, openmodelicas in ubuntu_openmodelica.items()
-        for openmodelica in openmodelicas
-        for python in pythons
-    }
+        images = {
+            OpenmodelicaPythonImage(
+                base="ijknabla/openmodelica",
+                ubuntu=ubuntu,
+                openmodelica=openmodelica,
+                python=python,
+            )
+            for ubuntu, openmodelicas in ubuntu_openmodelica.items()
+            for openmodelica in openmodelicas
+            for python in pythons
+        }
 
-    python0 = {image for image in images if image.python == pythons[0]}
-    ubuntu0 = {
-        image
-        for image in images
-        if image.openmodelica in map(itemgetter(0), ubuntu_openmodelica.values())
-    }
+        python0 = {image for image in images if image.python == pythons[0]}
+        ubuntu0 = {
+            image
+            for image in images
+            if image.openmodelica in map(itemgetter(0), ubuntu_openmodelica.values())
+        }
 
-    group0 = images & ubuntu0 & python0
-    group1 = images & ubuntu0 - python0
-    group2 = images - ubuntu0
+        group0 = images & ubuntu0 & python0
+        group1 = images & ubuntu0 - python0
+        group2 = images - ubuntu0
 
-    assert (group0 | group1 | group2) == images
+        assert (group0 | group1 | group2) == images
 
-    await gather(*(image.pull() for image in images), return_exceptions=True)
-    for group in [group0, group1, group2]:
-        await gather(*(image.build() for image in sorted(group)))
-    await gather(*(image.push() for image in images))
-    for image in sorted(images):
-        print(image)
+        await gather(*(image.pull() for image in images), return_exceptions=True)
+        for group in [group0, group1, group2]:
+            await gather(*(image.build() for image in sorted(group)))
+        await gather(*(image.push() for image in images))
+        for image in sorted(images):
+            print(image)
+
+    QCoreApplication()
+    QtAsyncio.run(impl(), keep_running=False)
 
 
 @asynccontextmanager
