8de5797ba6200d3683944798c9bd7a8c3d07f942
ijknabla <ijknabla@gmail.com>
2025-05-19 20:16:47 +0900

    Implement OpenModelica version order

--- neo/__init__.py
+++ neo/__init__.py
@@ -2,14 +2,18 @@ from __future__ import annotations
 
 import enum
 import re
+import sys
 from asyncio import subprocess
 from collections import defaultdict
 from collections.abc import AsyncIterator, Mapping, MutableSequence, Sequence
-from functools import total_ordering
+from enum import Enum, auto, unique
+from functools import partial, total_ordering
 from importlib.resources import as_file, files
-from typing import NewType, Self
+from typing import Any, Generic, NewType, Self, TypeVar
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, model_validator
+
+_print = partial(print, file=sys.stderr)
 
 _URI = NewType("_URI", str)
 _Ref = NewType("_Ref", str)
@@ -60,6 +64,133 @@ async def _iter_tags_in_remote2(uri: _URI) -> AsyncIterator[_Ref]:
             message = f"{buffer=!r}"
             raise ValueError(message)
         yield _Ref(matched.group("version").decode(encoding="ascii"))
+
+
+_SuffixType = TypeVar("_SuffixType")
+
+
+@total_ordering
+class SemVer(BaseModel, Generic[_SuffixType]):
+    major: int
+    minor: int
+    patch: int
+    suffix: _SuffixType
+
+    @model_validator(mode="before")
+    @classmethod
+    def _parse_string(cls, obj: Any) -> Any:
+        if not isinstance(obj, str):
+            return obj
+
+        pattern = re.compile(
+            r"v?"
+            r"(?P<major>\d+)\."
+            r"(?P<minor>\d+)\."
+            r"(?P<patch>\d+)"
+            r"(?P<suffix>.*)"
+        )
+
+        if (matched := pattern.match(obj)) is None:
+            message = f"{obj!r} does not matches pattern={pattern.pattern=!r}"
+            raise ValueError(message)
+
+        return {
+            "major": matched.group("major"),
+            "minor": matched.group("minor"),
+            "patch": matched.group("patch"),
+            "suffix": matched.group("suffix"),
+        }
+
+    @property
+    def tuple(self) -> tuple[int, int, int, _SuffixType]:
+        return self.major, self.minor, self.patch, self.suffix
+
+    def __lt__(self, other: Self, /) -> bool:
+        return self.tuple < other.tuple
+
+
+@unique
+class Dev(int, Enum):
+    yes = auto()
+    no = auto()
+
+
+@unique
+class Release(int, Enum):
+    none = auto()
+    alpha = auto()
+    beta = auto()
+
+
+@total_ordering
+@unique
+class InfType(Enum):
+    this = auto()
+
+    def __gt__(self, other: Any, /) -> bool:
+        return not isinstance(other, InfType)
+
+
+Inf = InfType.this
+
+
+@total_ordering
+class OpenModelicaSuffix(BaseModel):
+    dev: Dev
+    release: Release
+    micro: int | InfType
+
+    @model_validator(mode="before")
+    @classmethod
+    def _parse_string(cls, obj: Any) -> Any:
+        if not isinstance(obj, str):
+            return obj
+
+        sep = r"[.-]"
+        pattern = re.compile(
+            rf"^({sep}?(?P<dev>dev))?({sep}?(?P<release>alpha|beta))?({sep}?(?P<micro>\d+))?$"
+        )
+
+        if (matched := pattern.match(obj)) is None:
+            message = f"{obj!r} does not matches pattern={pattern.pattern!r}"
+            raise ValueError(message)
+
+        value = matched.group("dev")
+        if value is None:
+            dev = Dev.no
+        else:
+            dev = Dev.yes
+
+        value = matched.group("release")
+        if value is None:
+            release = Release.none
+        else:
+            release = Release[value]
+
+        micro: int | InfType
+        value = matched.group("micro")
+        if value is None:
+            micro = Inf
+        else:
+            micro = int(value)
+
+        return {
+            "dev": dev,
+            "release": release,
+            "micro": micro,
+        }
+
+    @property
+    def tuple(self) -> tuple[Dev, Release, int | InfType]:
+        return self.dev, self.release, self.micro
+
+    def __lt__(self, other: Self, /) -> bool:
+        return self.tuple < other.tuple
+
+
+PythonVersion = SemVer[str | None]
+# OpenModelicaVersion = SemVer[OpenModelicaVersion | str]
+OpenModelicaVersion = SemVer[OpenModelicaSuffix]
 
 
 class DockerBake(BaseModel):
